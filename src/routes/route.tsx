a8483894d5e43ae4f8fdd67f3ae095fb6b03155e
Anvar571 <abdurashidovanvar52@gmail.com>
2023-04-02 12:57:41 +0500

    initial state

---
+++ src/routes/route.tsx
@@ -0,0 +1,33 @@
+
+export default [
+    {
+        path:"/",
+        element: "",
+        children: [
+            {
+                path: "/",
+                element: ""
+            },
+            {
+                path: "/blog",
+                element:""
+            },
+            {
+                path: "/tag",
+                element:""
+            },
+            {
+                path: "/project",
+                element:""
+            },
+            {
+                path: "/about",
+                element:""
+            },
+        ]
+    },
+    {
+        path: "/footer",
+        element: ""
+    }
+]
