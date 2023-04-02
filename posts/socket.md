e5e74c1e00d86398bb1f10e2e1a6a464b125aa87
Anvar571 <abdurashidovanvar52@gmail.com>
2023-04-03 00:21:05 +0500

    initial state

---
+++ posts/socket.md
@@ -0,0 +1,7 @@
+---
+title: "Socket io"
+subtitle: "How to working Socket io "
+date: "2023-02-03"
+---
+
+## Socket io haqida qanday ishlashi 
--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import { useRoutes } from 'react-router-dom';
-import Route from './routes/route';
-
-function App() {
-  return useRoutes(Route)
-}
-
-export default App
